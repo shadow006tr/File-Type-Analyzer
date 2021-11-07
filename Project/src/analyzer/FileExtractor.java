3dcf26b15a4488b87f861f9089aeb902bf36caa2
Danny Wisotsky <shadow006tr@gmail.com>
2021-11-07 12:00:13 +0200

    Update FileExtractor.java

--- Project/src/analyzer/FileExtractor.java
+++ Project/src/analyzer/FileExtractor.java
@@ -13,10 +13,10 @@ public class FileExtractor {
     public void run (String folder, ArrayList<File> filesArray) {
 
         File currentFolder = new File(folder);                                      // Making a file object for
-        // the current folder
+                                                                                    // the current folder
 
         File[] files = currentFolder.listFiles();                                   // Getting all the files form
-        // the folder as file array.
+                                                                                    // the folder as file array.
 
         if (files != null) {                                                        // If we managed to get the files,
             for(File file : files) {                                                // we go on them one by one.
