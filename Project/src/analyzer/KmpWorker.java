5ad85b2536923bcc2a8aaab004bebcd0b51ed3b0
Danny Wisotsky <shadow006tr@gmail.com>
2021-11-07 11:59:51 +0200

    Update KmpWorker.java

--- Project/src/analyzer/KmpWorker.java
+++ Project/src/analyzer/KmpWorker.java
@@ -36,18 +36,18 @@ public class KmpWorker implements Runnable {
         int fileSize = fileBytes.length;                                            // Getting the byte array's length
         int patternLength = pattern.length();                                       // and the pattern's length.
         int count = 0;                                                              // A counter to check if we found
-        // the pattern.
+                                                                                    // the pattern.
         int i = 0;                                                                  // An index variable.
 
         int[] lps = new int[patternLength];                                         // Getting an LPS int array
-        // from the pattern
+                                                                                    // from the pattern
 
         boolean found = false;                                                      // A boolean to check if we
-        // found the pattern
+                                                                                    // found the pattern
         computeLPSArray(pattern, patternLength, lps);
 
         while (i < fileSize && !found) {                                            // Running on the array
-            // in search of the pattern.
+                                                                                    // in search of the pattern.
             if (pattern.charAt(count) == fileBytes[i]) {                            // If we find a part of the pattern,
                 count++;                                                            // we increment the counter
                 i++;                                                                // and the index.
@@ -56,9 +56,9 @@ public class KmpWorker implements Runnable {
                 found = true;                                                       // we can stop searching.
 
             } else if (i < fileSize && pattern.charAt(count) != fileBytes[i]) {     // if the next char is different
-                // from the one we are on in
-                // the pattern, abd we haven't
-                // reached the end of the file.
+                                                                                    // from the one we are on in
+                                                                                    // the pattern, abd we haven't
+                                                                                    // reached the end of the file.
                 if (count != 0) {                                                   // then if the counter is not zero,
                     count = lps[count - 1];                                         // we change it based of the lps
                 } else {                                                            // array.
@@ -67,10 +67,10 @@ public class KmpWorker implements Runnable {
             }                                                                       // in the byte array (file)
         }
         System.out.printf("%s: %s\n", file.getName(), found ? result : "Unknown file type");
-        // at the end we print the file's
-        // name and result if we found
-        // the pattern, or
-        // "Unknown file type" if we didn't
+                                                                                    // at the end we print the file's
+                                                                                    // name and result if we found
+                                                                                    // the pattern, or
+                                                                                    // "Unknown file type" if we didn't
     }
 
     /**
@@ -78,27 +78,27 @@ public class KmpWorker implements Runnable {
      * We will use that array in the KMP algorithm
      */
     private void computeLPSArray(String pattern, int size, int[] lps) {
-        int len = 0;                                                                // The length of the
-        // longest previous suffix
+        int length = 0;                                                             // The length of the
+                                                                                    // longest previous suffix
         int i = 1;                                                                  // Index in the array
         lps[0] = 0;                                                                 // The first element must be 0.
 
         while (i < size) {                                                          // running on the array
-            if (pattern.charAt(i) == pattern.charAt(len)) {                         // if the prefix letter is equal
-                // to the suffix letter,
-                len++;                                                              // we increment the length,
-                lps[i] = len;                                                       // writing it to the current
+            if (pattern.charAt(i) == pattern.charAt(length)) {                      // if the prefix letter is equal
+                                                                                    // to the suffix letter,
+                length++;                                                           // we increment the length,
+                lps[i] = length;                                                    // writing it to the current
                 i++;                                                                // position in the array,
-                // and then moving on the array.
+                                                                                    // and then moving on the array.
 
-            } else {                                                                // If pattern[i] != pattern[len],
-                if (len != 0) {                                                     // and the length isn't zero,
-                    len = lps[len - 1];                                             // we are writing the previous
-                    // length in the array.
+            } else {                                                                // If pattern[i] != pattern[length],
+                if (length != 0) {                                                  // and the length isn't zero,
+                    length = lps[length - 1];                                       // we are writing the previous
+                                                                                    // length in the array.
 
                 } else {                                                            // if the length is zero
-                    lps[i] = len;                                                   // we put zero in the current
-                    // index in the array
+                    lps[i] = length;                                                // we put zero in the current
+                                                                                    // index in the array
                     i++;                                                            // and move on it.
                 }
             }
